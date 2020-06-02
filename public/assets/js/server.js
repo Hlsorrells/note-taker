f74a74cbce1833e0830a682e0a02d1b3b258002a
hlsorrells <45104635+Hlsorrells@users.noreply.github.com>
2020-06-02 17:29:27 -0400

    created the HTML routes for both html files

--- public/assets/js/server.js
+++ public/assets/js/server.js
@@ -2,7 +2,8 @@
 const express = require('express');
 const serveStatic = require('serve-static');
 const app = express();
-const PORT = 3000;
+const path = require('path');
+const PORT = process.env.PORT || 3000;
 
 // Root director from which to serve static assets
 app.use(serveStatic('public'));
@@ -10,10 +11,18 @@ app.use(serveStatic('public'));
 // Data
 
 // HTML Routes
+app.get('/notes', function(req, res) {
+    res.sendFile(path.join(__dirname, '..', '..', 'notes.html'))
+});
+
+app.get('*', function(req, res) {
+    res.sendFile(path.join(__dirname, '..', '..', 'index.html'))
+})
 
 // API Routes
 
+
 // Listener
-app.listen(3000, function() {
-    console.log("App listening on PORT " + PORT);
+app.listen(PORT, function() {
+    console.log('App listening on PORT ' + PORT);
   });
